95afb11c3b45f92c4e05474dce6f47c769a6a1cc
Laratips <aashish201030@gmail.com>
2022-02-13 11:22:23 +0000

    update readme and add comments on config file

--- config/flatpickr.php
+++ config/flatpickr.php
@@ -1,7 +1,22 @@
 <?php
 
 return [
+    /**
+     * The url to be used to serve css file.
+     * If null, it will use the one shipped with package.
+     */
     'css_url' => env('FLATPICKR_CSS_URL', null),
+
+    /**
+     * The url to be used to serve js file.
+     * If null, it will use the one shipped with package.
+     */
     'js_url' => env('FLATPICKR_JS_URL', null),
+
+    /**
+     * Determines if the styles shipped with the package should be used.
+     * Setting it to false will remove the styling for the component.
+     * The flatpickr css will be untouched.
+     */
     'use_style' => env('FLATPICKR_USE_STYLE', true),
 ];
