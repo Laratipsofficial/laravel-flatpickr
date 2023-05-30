5b5631162ee54acd4280bee26953febbba403fe1
Laratipsofficial <Laratipsofficial@users.noreply.github.com>
2023-05-30 03:25:10 +0000

    Fix styling

--- src/Components/Flatpickr.php
+++ src/Components/Flatpickr.php
@@ -106,7 +106,7 @@ class Flatpickr extends Component
 
     private function value(): string|int|array|null
     {
-        if (!$this->value) {
+        if (! $this->value) {
             return null;
         }
 
@@ -159,7 +159,7 @@ class Flatpickr extends Component
 
     private function time24hr(): ?bool
     {
-        if (!$this->showTime) {
+        if (! $this->showTime) {
             return null;
         }
 
@@ -178,13 +178,13 @@ class Flatpickr extends Component
 
     private function throwValueExceptions()
     {
-        if (!$this->value) {
+        if (! $this->value) {
             return;
         }
 
         switch ($this->mode()) {
             case 'multiple':
-                if (!is_array($this->value)) {
+                if (! is_array($this->value)) {
                     throw new \Exception("The value must be array of dates or Carbon instances when multiple is set.");
                 }
 
@@ -199,11 +199,11 @@ class Flatpickr extends Component
                     return;
                 }
 
-                if (!is_string($this->value)) {
+                if (! is_string($this->value)) {
                     throw new \Exception("The value must be string when range is set.");
                 }
 
-                if (!Str::contains($this->value, ' to ')) {
+                if (! Str::contains($this->value, ' to ')) {
                     throw new \Exception("The two dates must be string and separated by ' to ' in between.");
                 }
 
